ef8fcf5f7f50d478fc85831fa6cabeacff9495bf
aswinkumarbaskar <93180931+aswinkumarbaskar@users.noreply.github.com>
2022-03-11 10:32:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const cors=require("cors");
+
+const app=express();
+app.use(cors());
+app.get("/test",(req,res)=>{
+ 
+    res.status(200).json({Mesaage:"success",Data:"Aswinkumar"})
+})
+
+app.get("/",(req,res)=>{
+    res.status(200).json({Mesaage:"success",Data:"Welcome to my testing...!!"})
+})
+
+app.listen(8080,()=>{
+    console.log("server Running");
+})
