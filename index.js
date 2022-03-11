6beb63c54d09c3f536cb8666fb8e7a16863efd56
aswinkumarbaskar <93180931+aswinkumarbaskar@users.noreply.github.com>
2022-03-11 10:38:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const cors=require("cors");
-
 const app=express();
+const port=process.env.PORT || 3000;
 app.use(cors());
 app.get("/test",(req,res)=>{
  
@@ -12,6 +12,6 @@ app.get("/",(req,res)=>{
     res.status(200).json({Mesaage:"success",Data:"Welcome to my testing...!!"})
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server Running");
 })
